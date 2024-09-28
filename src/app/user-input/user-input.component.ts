1984a3222a2558a2cb6f24ea532b4e89d2041d7e
Sachith-Innov8 <sachith.innov8fusion@gmail.com>
2024-09-28 12:58:47 +0530

    adding user inputs

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  intitailEnteredValue = '0';
+  investmentYearly = '0';
+  exReturn = '5';
+  duration = '10';
 
+  onSubmit() {
+    console.log(this.intitailEnteredValue);
+    console.log(this.investmentYearly);
+    console.log(this.duration);
+    console.log(this.exReturn);
+  }
 }
